8f6cb187a196d1697ccddfdf73a0aedd0d0cbed6
Irfan-Ayub <irfanayub995@gmail.com>
2025-07-12 10:14:04 +0500

    Fixed JSON issue with Full name

--- api/user.go
+++ api/user.go
@@ -21,7 +21,7 @@ type userResponse struct {
 type createUserRequest struct {
 	Username string `json:"username" binding:"required,alphanum"`
 	Password string `json:"password" binding:"required,min=6"`
-	FullName string `json:"full_name" binding:"required`
+	FullName string `json:"full_name" binding:"required"`
 	Email    string `json:"email" binding:"required,email"`
 }
 

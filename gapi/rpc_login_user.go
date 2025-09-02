4f026facef661dbe89f0b50eea250ca2ce268aa6
Muhammad Irfan Ayub <irfan.ayub@securiti.ai>
2025-09-02 23:26:55 +0500

    Added metadata extracter for ClientIP and Agent

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -13,7 +13,6 @@ import (
 )
 
 func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
-
 	user, err := server.store.GetUser(ctx, req.GetUsername())
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -31,7 +30,6 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 		user.Username,
 		server.config.AccessTokenDuration,
 	)
-
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "failed to create access token: %s", err)
 	}
@@ -40,21 +38,20 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 		user.Username,
 		server.config.RefreshTokenDuration,
 	)
-
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "failed to create refresh token: %s", err)
 	}
 
+	mtdt := server.extractMetadata(ctx)
 	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
 		ID:           refreshPayload.ID,
 		Username:     user.Username,
 		RefreshToken: refreshToken,
-		UserAgent:    "placeholder",
-		ClientIp:     "placeholder",
+		UserAgent:    mtdt.UserAgent,
+		ClientIp:     mtdt.ClientIP,
 		IsBlocked:    false,
 		ExpiresAt:    refreshPayload.ExpiredAt,
 	})
-
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "failed to create session: %s", err)
 	}
